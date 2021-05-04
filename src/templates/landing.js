2317b00fb396586a69ff4166d01d84bb78266cb0
Stackbit Code Editor <projects@stackbit.com>
2021-05-04 13:55:44 +0000

    landing.js: changed

--- src/templates/landing.js
+++ src/templates/landing.js
@@ -24,7 +24,6 @@ export default class Landing extends React.Component {
                 return (
                   <Component key={section_idx} {...this.props} section={section} site={this.props.pageContext.site} />
                 )
-                <div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/536784380?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="Secret Network - A Privacy Hub for the Blockchain Universe"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
             })}
             </Layout>
         );
